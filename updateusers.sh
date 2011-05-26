1d2d32c33c762a83a068bf9ac33a1d40acc8ba92
blarghmatey <tmacey@reso.com>
2011-05-26 05:53:34 -0700

    Edited updateusers.sh via GitHub

--- updateusers.sh
+++ updateusers.sh
@@ -2,7 +2,7 @@
 
 FILE=/etc/postfix/userlist
 
-ldapsearch -x -D "uid=zimbra,cn=admins,cn=zimbra" -H ldap://mailserver.resolution.resodirect.com:389 -w T2saegr08+ -b dc=com | grep mail: | sed 's/mail:\ //' | sed 's/$/\ OK/' > $FILE
+ldapsearch -x -D "uid=zimbra,cn=admins,cn=zimbra" -H ldap://mailserver.resolution.resodirect.com:389 -w $PASS -b dc=com | grep mail: | sed 's/mail:\ //' | sed 's/$/\ OK/' > $FILE
 
 if [ ! -s $FILE ];
  then exit
